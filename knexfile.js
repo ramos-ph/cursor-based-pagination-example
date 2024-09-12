91731ab572909760e18f3e16ee97f57f8c8ef906
Pedro Ramos <pedroramos.contato@outlook.com.br>
2024-09-12 11:21:20 -0300

    Add knexfile

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+/**
+ * @type { import("knex").Knex.Config }
+ */
+module.exports = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./db.sqlite3",
+  },
+  migrations: {
+    directory: path.resolve(__dirname, "src", "database", "migrations"),
+  },
+};
