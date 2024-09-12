25c0f18a59c5795a3889d874eba711027a16a8f6
Pedro Ramos <pedroramos.contato@outlook.com.br>
2024-09-12 11:21:28 -0300

    Add entries migration

---
+++ src/database/migrations/20240912141821_create_entries.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("entries", (table) => {
+    table.increments("id");
+
+    table.string("description").notNullable();
+    table.string("operation").notNullable(); // credit | debit
+    table.integer("value").notNullable();
+
+    table.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("entries");
+};
