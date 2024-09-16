ca200a2e1e85e0eb761996b964a9d903d06b5bbf
Pedro Ramos <pedroramos.contato@outlook.com.br>
2024-09-16 18:21:08 -0300

    Remove orderBy

--- src/services/paginate-entries.js
+++ src/services/paginate-entries.js
@@ -1,7 +1,7 @@
 const knex = require("../database/knex");
 
 exports.paginateEntries = async ({ first, last, before, after } = {}) => {
-  const entries = await knex("entries").select("*").orderBy("id", "desc");
+  const entries = await knex("entries").select("*");
 
   const [{ count }] = await knex("entries").count({ count: "*" });
 
