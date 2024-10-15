881beec64f313713506574c05dd07e6fda97b1b5
Pedro Henrique <phenrique@godaddy.com>
2024-10-15 10:39:23 -0300

    Add validations

--- src/middlewares/graphql-handler.js
+++ src/middlewares/graphql-handler.js
@@ -4,6 +4,11 @@ const { paginateEntries } = require("../services/paginate-entries");
 
 const root = {
   async entries({ first = 10, last, before, after }) {
+    if (first && first < 0)
+      throw new Error('Argument `first` cannot be negative.');
+    if (last && last < 0) 
+      throw new Error('Argument `last` cannot be negative.')
+    
     const { data: entries, total_records } = await paginateEntries({
       first,
       last,
@@ -19,8 +24,8 @@ const root = {
     return {
       edges: edges,
       pageInfo: {
-        startCursor: edges.at(0).cursor,
-        endCursor: edges.at(-1).cursor,
+        startCursor: edges.at(0)?.cursor || null,
+        endCursor: edges.at(-1)?.cursor || null,
         hasNextPage: first < total_records,
         hasPreviousPage: !!after,
       },
