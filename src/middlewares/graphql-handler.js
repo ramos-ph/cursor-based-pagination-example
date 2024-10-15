73596f20e4718169f5e4f9700abb7966baaeeb77
ramos-ph <pedroramos.contato@outlook.com.br>
2024-10-15 15:38:57 -0300

    Move logic to paginate-entries

--- src/middlewares/graphql-handler.js
+++ src/middlewares/graphql-handler.js
@@ -4,33 +4,12 @@ const { paginateEntries } = require("../services/paginate-entries");
 
 const root = {
   async entries({ first = 10, last, before, after }) {
-    if (first && first < 0)
-      throw new Error('Argument `first` cannot be negative.');
-    if (last && last < 0) 
-      throw new Error('Argument `last` cannot be negative.')
-    
-    const { data: entries, totalRecords } = await paginateEntries({
+    return await paginateEntries({
       first,
       last,
       before,
       after,
     });
-
-    const edges = entries.map((entry) => ({
-      node: entry,
-      cursor: entry.id,
-    }));
-
-    return {
-      edges: edges,
-      pageInfo: {
-        startCursor: edges.at(0)?.cursor || null,
-        endCursor: edges.at(-1)?.cursor || null,
-        hasNextPage: edges.length >= first,
-        hasPreviousPage: !!after,
-      },
-      totalCount: totalRecords,
-    };
   },
 };
 
--- src/services/paginate-entries.js
+++ src/services/paginate-entries.js
@@ -1,6 +1,11 @@
 const knex = require("../database/knex");
 
 exports.paginateEntries = async ({ first, last, before, after } = {}) => {
+  if (first && first < 0)
+    throw new Error('Argument `first` cannot be negative.');
+  if (last && last < 0) 
+    throw new Error('Argument `last` cannot be negative.')
+
   const query = knex("entries").limit(first).select("*");
 
   if (after) {
@@ -9,14 +14,21 @@ exports.paginateEntries = async ({ first, last, before, after } = {}) => {
 
   const entries = await query;
 
+  const edges = entries.map((entry) => ({
+    node: entry,
+    cursor: entry.id,
+  }));
+
   const [{ count: totalRecords }] = await knex("entries").count({ count: "*" });
 
   return {
-    data: entries.map((entry) => ({
-      ...entry,
-      createdAt: entry.created_at.toString(),
-      updatedAt: entry.updated_at.toString(),
-    })),
-    totalRecords: totalRecords,
+    edges: edges,
+    pageInfo: {
+      startCursor: edges.at(0)?.cursor || null,
+      endCursor: edges.at(-1)?.cursor || null,
+      hasNextPage: edges.length >= first,
+      hasPreviousPage: !!after,
+    },
+    totalCount: totalRecords,
   };
 };
